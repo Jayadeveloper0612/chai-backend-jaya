94d2732f22c2ff72f79932830775364aa37cb714
Jayadeveloper0612 <jayakurre8@gmail.com>
2025-01-30 13:05:49 +0530

    setup models : user, video model  and also working hashPassword , tokenGenerate setup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import dotenv from "dotenv";
-import colors from "colors";
-import connectDB from "./config/db.js";
+import dotenv from "dotenv"
+import colors from "colors"
+import connectDB from "./config/db.js"
 // import app from "./app.js";
-import { app } from "./app.js";
+import { app } from "./app.js"
 
-dotenv.config({ path: "./env" });
+dotenv.config({ path: "./env" })
 
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 8001
 
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`\n Server is running at PORT : ${PORT}`.bgGrey.white);
-    });
+      console.log(`\n Server is running at PORT : ${PORT}`.bgGrey.white)
+    })
   })
   .catch((err) => {
-    console.log("MONGO DB Connection Failed !!! ", err);
-  });
+    console.log("MONGO DB Connection Failed !!! ", err)
+  })
