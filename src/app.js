fe338c7c29ac757a77668f6bdcc5dbabd6ac7fa8
Jayadeveloper0612 <jayakurre8@gmail.com>
2025-02-03 10:53:38 +0530

    Logic build - add register controller

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-
+import bodyParser from "body-parser"
 const app = express()
 
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }))
@@ -9,6 +9,7 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use(bodyParser())
 
 //routes import
 import userRoutes from "./routes/user.routes.js"
